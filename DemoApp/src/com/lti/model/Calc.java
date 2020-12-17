765f36fbe3c9135e076365a0358d39f337b2c58b
sivarajgs <sivarajfairlands@gmail.com>
2020-12-17 18:11:48 +0530

    br2

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,5 +4,8 @@ public class Calc {
 public void add(int num1,int num2){
 	System.out.println("Sum="+(num1+num2));
 }
+public void dd(int num1,int num2){
+	System.out.println("div="+(num1/num2));
+}
 
 }
