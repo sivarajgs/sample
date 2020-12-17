e2769dffdc1a67755b60de7faaa5086c62aa7acf
sivarajgs <sivarajfairlands@gmail.com>
2020-12-17 18:09:24 +0530

    branch

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,8 +4,5 @@ public class Calc {
 public void add(int num1,int num2){
 	System.out.println("Sum="+(num1+num2));
 }
-public void subs(int num1,int num2){
-	System.out.println("diff="+(num1-num2));
-}
 
 }
