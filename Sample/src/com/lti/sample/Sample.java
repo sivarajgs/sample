f1dbf18c4b25d61f561d06563fc13baded8b3ebb
sivarajgs <sivarajfairlands@gmail.com>
2020-12-17 18:04:54 +0530

    comiting and pushing sample

---
+++ Sample/src/com/lti/sample/Sample.java
@@ -0,0 +1,12 @@
+package com.lti.sample;
+
+public class Sample {
+
+	public void add(int num1,int num2){
+		System.out.println("Sum="+(num1+num2));
+	}
+	public void subs(int num1,int num2){
+		System.out.println("diff="+(num1-num2));
+	}
+
+	}
